e4dfa586846639e51e02a2feb0d6b2fa09f6670d
AJ Leichner <leichneraj@s.dcsdk12.org>
2021-09-09 22:05:11 -0600

    Add comments and fix recurrment error in exception handling.

--- Homework/Homework3/RandomAddition.java
+++ Homework/Homework3/RandomAddition.java
@@ -1,7 +1,17 @@
+/*
+
+    AJ Leichner
+    Douglas Lundin
+    CSC160-179 - Computer Science 1: Java
+    12 September 2021
+
+*/
+
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class RandomAddition {
+public class RandomAddition { // Gets two random numbers, adds them, then asks the player for the answer to the sum of the numbers
 
     public static void main(String[] args) {
         Random random = new Random();
@@ -21,8 +31,8 @@ public class RandomAddition {
             System.out.print("What is " + number1 + " + " + number2 + " ? ");
             answer = input.nextInt();
         } catch(Exception e) {           // If the code can't execute because of the wrong input type, it completes a print statement explaining so.
-            System.out.println("Incorrect dataType. Please enter in an integer.\n");
-            main(args);
+            System.out.println("Incorrect dataType. Please enter in an integer.");
+            System.exit(0); // Exits the program
         }
 
         if(number1 + number2 == answer) {
