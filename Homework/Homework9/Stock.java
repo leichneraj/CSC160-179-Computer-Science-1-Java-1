2879d230ad443a6868b3d487a16bfa8ae3977cc6
AJ Leichner <leichneraj@s.dcsdk12.org>
2021-11-02 22:42:54 -0600

    Create a program which eates a stock option then can display the information involved.

---
+++ Homework/Homework9/Stock.java
@@ -0,0 +1,25 @@
+public class Stock {
+
+    private String symbol, name;                // Variables
+    private double prevClosPrice, currentPrice;
+
+
+    public Stock(String symbol, String name, double prevClosPrice, double currentPrice) { // Constructor
+        this.symbol = symbol;
+        this.name = name;
+        this.prevClosPrice = prevClosPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    // Setters - Name and Symbol not needed since those should be the same for the existence of the company
+    public void setCurrentPrice(double n) { setPrevClosPrice(this.currentPrice); this.currentPrice = n;  } // Sets new price and calls setPrevClosPrice giving the old value
+    public void setPrevClosPrice(double n) { this.prevClosPrice = n; }
+
+    // Getters
+    public String getSymbol() { return this.symbol; }
+    public String getName() { return this.name; }
+    public double getPrevClosPrice() { return this.prevClosPrice; }
+    public double getcurrentPrice() { return this.currentPrice; }
+    public double getChangePercent() { return ((prevClosPrice / currentPrice) * 100) - 100.0; } // Returns the percentage of change, positive is increase, negative is decrease
+
+}
