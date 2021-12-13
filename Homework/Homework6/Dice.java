31b618292824e1b6e84fccedbb23df37f93603bf
AJ Leichner <leichneraj@s.dcsdk12.org>
2021-12-12 22:35:50 -0700

    Fix random roll game program for 6.30 on page 243 & add commenting.

--- Homework/Homework6/Dice.java
+++ Homework/Homework6/Dice.java
@@ -7,59 +7,50 @@
 
     A program that asks completes the requirements for the 6.30 problem on page 243.
 
-    Not for Doug:
-    This is a terrible program I have written. My deadline is coming due and this 
-    program is not nearly as good as I can make it. I'm sorry but I've been very busy
-    and needed to get this done.
-
 */
 
 import java.util.Random;
 
-import javax.management.monitor.GaugeMonitor;
-
 public class Dice {
 
     public static void main(String[] args) {
-        game(-1);
-    }
-
-        static int repeats = 0;
-    public static void game(int previousPoints) {
         Random random = new Random();
 
-        int firstN, secondN = 0;
-        boolean success = false;
-        while(success == false) {
+        int firstN = 0, secondN = 0; // Initializes dice
+        while((firstN + secondN) != 2 && (firstN + secondN) != 3 && (firstN + secondN) != 12 && (firstN + secondN) != 7 && (firstN + secondN) != 11) { // While total != special numbers defined in problem
             int points;
             firstN = random.nextInt(6) + 1;
             secondN = random.nextInt(6) + 1;
-            
-            int total = firstN + secondN;
-            System.out.println("You rolled " + firstN + " + " + secondN + " = " + total);
 
-            if(total == 7 || total == 11) {
-                System.out.println("You win!");
-                success = true;
-            } else if(total == 2 || total == 3 || total == 12) {
-                System.out.println("You lose!");
-                success = true;
-            } else {
-                points = total;
-                if(repeats == 0) {
-                    System.out.println("point is " + points);
-                    repeats++;
-                    game(points);
-                } else if ((repeats == 1 && previousPoints != points) && success == false) {
+            System.out.println("You rolled " + firstN + " + " + secondN + " = " + (firstN + secondN) );
+            if((firstN + secondN) != 2 && (firstN + secondN) != 3 && (firstN + secondN) != 12 && (firstN + secondN) != 7 && (firstN + secondN) != 11) { // While total != special numbers defined in problem
+                points = firstN + secondN; // Initalizes points
+                do { // Do-while for as long as the total doesn't equal points or 7
+                    System.out.println("Point is " + points);
+                    
+                    firstN = random.nextInt(6) + 1;
+                    secondN = random.nextInt(6) + 1;
+
+                    System.out.println("You rolled " + firstN + " + " + secondN + " = " + (firstN + secondN) );
+                } while((firstN + secondN) != 7 && (firstN + secondN) != points);
+
+                if(firstN + secondN == 7) { // Checks which result after do-while ends
                     System.out.println("You lose!");
-                    success = true;
-                    System.exit(1);
+                    return; // Stops the program if the total equals 7
                 } else {
                     System.out.println("You win!");
-                    success = true;
+                    return; // Stops the program if the total equals the points
                 }
             }
+
+            //Checks result of first roll if there is no point rolled
+            if((firstN + secondN) == 2 || (firstN + secondN) == 3 || (firstN + secondN) == 12) 
+                System.out.println("You lose!");
+            else if((firstN + secondN) == 7 || (firstN + secondN) == 11)
+                System.out.println("You win!");
+
         }
+
     }
 
 }
